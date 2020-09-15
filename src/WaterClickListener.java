de62c979af443e66646ded192c4cebe0eb089422
Zongo Maqutu <MQTZON001@myuct.ac.za>
2020-09-15 17:36:45 +0200

    Version 4: Tested addWater() & addWaterOnScreen(). Both work as expected

--- src/WaterClickListener.java
+++ src/WaterClickListener.java
@@ -8,31 +8,32 @@ public class WaterClickListener extends MouseAdapter {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		System.out.println(e.getX() + "," + e.getY() + " has a height of " + Terrain.getGrid()[e.getX()][e.getY()].getHeight() );
-		//System.out.println(Terrain.height[e.getX()][e.getY()].getHeight());
-		//System.out.println(Terrain.getGrid(e.getX(),e.getY()).getHeight());
-		System.out.println(Terrain.getGrid()[e.getX()][e.getY()].getHeight());
 
-		Terrain.getGrid()[e.getX()][e.getY()].setWaterDepth(3);
-		System.out.println(Terrain.getGrid()[e.getX()][e.getY()].getWaterSurface());
-
-		//addWater(getX(), getY());
-		Color waterColor = new Color(106,195,255,150);
-		//addWaterOnScreen(e.getX(),e.getY(), waterColor);
-
-		for(int i = e.getX()- 3; i <= e.getX() + 3; i++)
-		{
-			for(int j = e.getY() - 3; j <= e.getY() + 3; j++)
-			{
-				Flow.fp.land.getWaterImage().setRGB(i,j,waterColor.getRGB());
+		//Terrain.getGrid()[e.getX()][e.getY()].setWaterDepth(3);
+		//System.out.println(Terrain.getGrid()[e.getX()][e.getY()].getWaterSurface());
+		for(int i = e.getX() - 3; i <= e.getX() + 3; i++){
+			for(int j = e.getY() -3; j <= e.getY() + 3;j++){
+				System.out.println("Coordinates : " + i + "," + j +" WaterDepth Before: " + Terrain.getGrid()[i][j].getWaterDepth()
+						+ "\n WaterSurface Before: " + Terrain.getGrid()[i][j].getHeight());
 			}
 		}
-		//Flow.fp.land.getImage().setRGB(e.getX(), e.getY(), waterColor.getRGB());
-		Flow.fp.repaint();
+		addWater(e.getX(), e.getY());
+		Color waterColor = new Color(106,195,255,150);
+		addWaterOnScreen(e.getX(),e.getY(), waterColor);
+		for(int i = e.getX() - 3; i <= e.getX() + 3; i++)
+		{
+			for(int j = e.getY() -3; j <= e.getY() + 3;j++)
+			{
+				System.out.println("Coordinates : " + i + "," + j +" WaterDepth After: " + Terrain.getGrid()[i][j].getWaterDepth()
+                                                + "\n WaterSurface After: " + Terrain.getGrid()[i][j].getWaterSurface());
+			}
+		}
+
 	}
 	public void addWaterOnScreen(int x, int y, Color waterColor){
 		for(int i = x - 3; i <= x + 3; i++){
-			for(int j = y - 3; i <= y + 3;j++){
-				Flow.fp.land.getImage().setRGB(i,j,waterColor.getRGB());
+			for(int j = y - 3; j <= y + 3;j++){
+				Flow.fp.land.getWaterImage().setRGB(i,j,waterColor.getRGB());
 			}
 		}
 		Flow.fp.repaint();
@@ -41,68 +42,10 @@ public class WaterClickListener extends MouseAdapter {
 		System.out.println("lol");
 
 
-		//set row 1 water depth
-		Terrain.getGrid()[x-3][y-3].setWaterDepth(3);
-		Terrain.getGrid()[x-2][y-3].setWaterDepth(3);
-		Terrain.getGrid()[x-1][y-3].setWaterDepth(3);
-		Terrain.getGrid()[x][y-3].setWaterDepth(3);
-		Terrain.getGrid()[x+1][y-3].setWaterDepth(3);
-		Terrain.getGrid()[x+2][y-3].setWaterDepth(3);
-		Terrain.getGrid()[x+3][y-3].setWaterDepth(3);
-
-		//set row 2 water depth
-		Terrain.getGrid()[x-3][y-2].setWaterDepth(3);
-		Terrain.getGrid()[x-2][y-2].setWaterDepth(3);
-		Terrain.getGrid()[x-1][y-2].setWaterDepth(3);
-		Terrain.getGrid()[x][y-2].setWaterDepth(3);
-		Terrain.getGrid()[x+1][y-2].setWaterDepth(3);
-		Terrain.getGrid()[x+2][y-2].setWaterDepth(3);
-		Terrain.getGrid()[x+3][y-2].setWaterDepth(3);
-
-		//set row 3 water depth
-		Terrain.getGrid()[x-3][y-1].setWaterDepth(3);
-		Terrain.getGrid()[x-2][y-1].setWaterDepth(3);
-		Terrain.getGrid()[x-1][y-1].setWaterDepth(3);
-		Terrain.getGrid()[x][y-1].setWaterDepth(3);
-		Terrain.getGrid()[x+1][y-1].setWaterDepth(3);
-		Terrain.getGrid()[x+2][y-1].setWaterDepth(3);
-		Terrain.getGrid()[x+3][y-1].setWaterDepth(3);
-
-		//set row 4 water depth
-		Terrain.getGrid()[x-3][y].setWaterDepth(3);
-		Terrain.getGrid()[x-2][y].setWaterDepth(3);
-		Terrain.getGrid()[x-1][y].setWaterDepth(3);
-		Terrain.getGrid()[x][y].setWaterDepth(3);
-		Terrain.getGrid()[x+1][y].setWaterDepth(3);
-		Terrain.getGrid()[x+2][y].setWaterDepth(3);
-		Terrain.getGrid()[x+3][y].setWaterDepth(3);
-
-		//set row 5 water depth
-		Terrain.getGrid()[x-3][y+1].setWaterDepth(3);
-		Terrain.getGrid()[x-2][y+1].setWaterDepth(3);
-		Terrain.getGrid()[x-1][y+1].setWaterDepth(3);
-		Terrain.getGrid()[x][y+1].setWaterDepth(3);
-		Terrain.getGrid()[x+1][y+1].setWaterDepth(3);
-		Terrain.getGrid()[x+2][y+1].setWaterDepth(3);
-		Terrain.getGrid()[x+3][y+1].setWaterDepth(3);
-
-		//set row 6 water depth
-		Terrain.getGrid()[x-3][y+2].setWaterDepth(3);
-		Terrain.getGrid()[x-2][y+2].setWaterDepth(3);
-		Terrain.getGrid()[x-1][y+2].setWaterDepth(3);
-		Terrain.getGrid()[x][y+2].setWaterDepth(3);
-		Terrain.getGrid()[x+1][y+2].setWaterDepth(3);
-		Terrain.getGrid()[x+2][y+2].setWaterDepth(3);
-		Terrain.getGrid()[x+3][y+2].setWaterDepth(3);
-
-		//set row 7 water depth
-		Terrain.getGrid()[x-3][y+3].setWaterDepth(3);
-		Terrain.getGrid()[x-2][y+3].setWaterDepth(3);
-		Terrain.getGrid()[x-1][y+3].setWaterDepth(3);
-		Terrain.getGrid()[x][y+3].setWaterDepth(3);
-		Terrain.getGrid()[x+1][y+3].setWaterDepth(3);
-		Terrain.getGrid()[x+2][y+3].setWaterDepth(3);
-		Terrain.getGrid()[x+3][y+3].setWaterDepth(3);
-
+		for(int i = x - 3; i <= x + 3; i++){
+			for(int j = y - 3; j <= y + 3;j++){
+				Terrain.getGrid()[i][j].setWaterDepth(3);
+			}
+		}
 	}
 }
